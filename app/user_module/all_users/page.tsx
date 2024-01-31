ad73fb548e915d85f5ae309cad354db2908bb13a
hamskid-id <lawalhamzat27@gmail.com>
2024-01-31 14:29:55 -0800

    fixed ui bug

--- app/user_module/all_users/page.tsx
+++ app/user_module/all_users/page.tsx
@@ -1,13 +1,19 @@
 import { DashBoardLayout } from "@/components/customui/dashboard/dashboardLayout";
 import { All_Users } from "@/components/customui/userModule/allUsers";
+import { cookies } from "next/headers";
 import React from "react";
+
+const cookieStore = cookies();
+const storedItem = cookieStore.get("datahubToken");
 
 type Props = {};
 
 export const page = (props: Props) => {
   return (
     <main>
-      <DashBoardLayout>
+      <DashBoardLayout
+        firstname={storedItem?.value && JSON.parse(storedItem?.value)?.user?.firstname}
+      >
         {/* <All_Users /> */}
         ufuyy
       </DashBoardLayout>
