7b1ef8cd2750d76490bc61472fb3f73e30e0b43e
hamskid-id <lawalhamzat27@gmail.com>
2024-03-17 21:35:20 -0700

    Added credit/debit account Features

---
+++ app/dashboard/credit&DebitUser/page.tsx
@@ -0,0 +1,9 @@
+import { Credit_And_Debit } from "@/components/customui/credit&debit/credit&debit";
+
+export default async function Credit_DebitPage() {
+    return (
+      <>
+        <Credit_And_Debit />
+      </>
+    )
+  }
